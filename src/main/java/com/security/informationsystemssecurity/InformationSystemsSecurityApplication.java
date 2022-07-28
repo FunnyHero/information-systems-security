4b67045f383b91bc6ca0e8a76bd65a4dce6f80a8
FunnyHero <hgdrtttr@gmail.com>
2022-07-28 18:13:59 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/security/informationsystemssecurity/InformationSystemsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.informationsystemssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationSystemsSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/informationsystemssecurity/InformationSystemsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.informationsystemssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationSystemsSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
