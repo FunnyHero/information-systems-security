583c3567e7004d20ea9bcbb2cbff4cbb540c08a9
FunnyHero <hgdrtttr@gmail.com>
2022-10-12 23:13:05 +0300

    Fix calculation from form

--- src/main/java/com/security/informationsystemssecurity/controller/SecurityGradeController.java
+++ src/main/java/com/security/informationsystemssecurity/controller/SecurityGradeController.java
@@ -1,6 +1,7 @@
 package com.security.informationsystemssecurity.controller;
 
-import com.security.informationsystemssecurity.model.dto.StageDTO;
+import com.security.informationsystemssecurity.model.dto.DirectionDTO;
+import com.security.informationsystemssecurity.model.enums.DirectionEnum;
 import com.security.informationsystemssecurity.service.SecurityGradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.util.List;
 
 import static com.security.informationsystemssecurity.model.enums.DirectionEnum.valueOf;
 
@@ -29,38 +28,25 @@ public class SecurityGradeController {
 
     @PostMapping
     public String getSecurityGrade(Model model, HttpServletRequest request) {
-//        List<StageDTO> stageDTOS = Arrays.asList(
-//                new StageDTO(
-//                        new BasicsDTO(121,new BigDecimal("0.2"),new BigDecimal("0.5"),null,null),
-//                        new BasicsDTO(122,new BigDecimal("0.3"),new BigDecimal("0.4"),null,null),
-//                        new BasicsDTO(123,new BigDecimal("0.1"),new BigDecimal("0.9"),null,null),
-//                        new BasicsDTO(124,new BigDecimal("0.4"),new BigDecimal("0.1"),null,null),
-//                        null
-//                ),
-//                new StageDTO(
-//                        new BasicsDTO(131,new BigDecimal("0.2"),new BigDecimal("0.2"),null,null),
-//                        new BasicsDTO(132,new BigDecimal("0.2"),new BigDecimal("0.3"),null,null),
-//                        new BasicsDTO(133,new BigDecimal("0.2"),new BigDecimal("0.4"),null,null),
-//                        new BasicsDTO(134,new BigDecimal("0.2"),new BigDecimal("0.5"),null,null),
-//                        null
-//                )
-//        );
-//        securityGradeService.save(stageDTOS);
-        securityGradeService.save(securityGradeService.findByDirection(valueOf(request.getParameterValues("direction")[0])));
+        DirectionEnum direction = valueOf(request.getParameterValues("direction")[0]);
+
         BigDecimal requiredSecurityProfile = new BigDecimal(request.getParameterValues(REQUIRED_SECURITY_PROFILE)[0]);
         requiredSecurityProfile = requiredSecurityProfile.divide(new BigDecimal("100"));
-        //TODO putMapping изменение параметров в матрице для расчета (сейчас при расчете NPE)
-//        securityGradeService.countSecurityGrade(requiredSecurityProfile);
         model.addAttribute(REQUIRED_SECURITY_PROFILE, requiredSecurityProfile);
-        model.addAttribute("stages", securityGradeService.findAll());
-//        model.addAttribute("qualityGrade", securityGradeService.getQualityGrade());
-//        model.addAttribute("quantityGrade", securityGradeService.getQuantityGrade());
+
+        DirectionDTO directionDTO = new DirectionDTO(securityGradeService.findByDirection(direction), direction);
+        model.addAttribute("stages", directionDTO);
         return "securityGrade";
     }
 
     @PostMapping("securityGrade")
-    public String countSecurityGrade(@ModelAttribute("stages")List<StageDTO> stages) {
-        System.out.println(stages.toString());
+    public String countSecurityGrade(@ModelAttribute("stages") DirectionDTO stages,
+                                     @ModelAttribute(REQUIRED_SECURITY_PROFILE) BigDecimal requiredSecurityProfile,
+                                     Model model) {
+        securityGradeService.save(stages.getStageDTOS());
+        securityGradeService.countSecurityGrade(requiredSecurityProfile);
+        model.addAttribute("qualityGrade", securityGradeService.getQualityGrade());
+        model.addAttribute("quantityGrade", securityGradeService.getQuantityGrade());
         return "countSecurityGrade";
     }
 }
